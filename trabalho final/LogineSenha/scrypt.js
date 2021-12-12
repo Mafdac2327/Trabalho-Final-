5ff9bd86b6959c6e521ace46625813d44f311892
Mafdac2327 <90983810+Mafdac2327@users.noreply.github.com>
2021-12-12 10:54:57 -0300

    Add files via upload

--- trabalho final/LogineSenha/scrypt.js
+++ trabalho final/LogineSenha/scrypt.js
@@ -1,24 +1,31 @@
 function mostrarsenha( ){
-   let divnova = document.createElement( "div" );
-   let ret = document.querySelector( "#senhaUsuario" );
-   let inputsenha = ret.value;
+   
+   let inputnome = document.querySelector( "#nome" );
+   let ret = document.querySelector( "#NomedoUsuario" );
+   let textNovo = document.createTextNode( `Bem vindo ${ ret.value } )`);
+   let divnova = document.createElement ("div");
+   s
+   divnova.appendChild( textNovo );
+   document.body.appendChild( divnova );
+    
+   let ret2 = document.querySelector( "#senhaUsuario" );
+   let inputsenha = ret2.value;
    const senha = "ola";
    console.log( inputsenha )
    let lk = "Clic Aki";
    if( inputsenha == senha ){
     
-    window.location.href = "../src/trabalhofinal.html";
+    window.location.href = "src/trabalhofinal.html";
    
    }
    else{
     alert("Senha incorreta")
-
+    
    }
 }
 let botaooK = document.querySelector( "#enviar" );
 
-botaooK.onClick = mostrarsenha;
-
+botaooK.onclick = mostrarsenha;
 
 // console.log( botaooK );
 // botaooK.onclick = mostrarsenha;
